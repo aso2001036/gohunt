d85804804937083c4b11f32dd424cea2911ecad2
Aso2001008 <2001008@s.asojuku.ac.jp>
2022-11-07 10:10:10 +0900

    テスト

---
+++ conect.php
@@ -0,0 +1,7 @@
+<?php
+$pdo = new PDO(
+        'mysql:host=mysql207.phy.lolipop.lan;
+        dbname=LAA1290570-gohunt;charaset=utf8',
+        'LAA1290570',
+        'gohunt');
+?>
---
+++ signUp/signup.php
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <meta charset = "utf-8">
+    <link rel = "stylesheet" href = "./css/signup.css">
+</head>
+<body>
+<h2>新規登録</h2>
+<div class = "IPhone">
+    <form action = "../signUpCheck/reg_confirm.html" method = "post">
+        <div class = "Form">
+            <p>ユーザーネーム</p>
+            <input type = "text" name = "user">
+            <p>メールアドレス</p>
+            <input type = "text" name = "e-mail">
+            <p>パスワード</p>
+            <input type = "text" name = "pass">
+            <p>パスワード（再）</p>
+            <input type = "text" name = "repass">
+            <p>性別</p>
+        </div>
+        <div class = "ra">
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "man">　男　</label>
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "woman">　女　</label>
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "other">　その他　</label>
+        </div>
+        <br>
+    <button class = "SignUp">確認</button><br>
+    <br>
+    </form>
+</div>
+</body>
+=======
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <meta charset = "utf-8">
+    <link rel = "stylesheet" href = "./css/signup.css">
+</head>
+<body>
+<h2>新規登録</h2>
+<div class = "IPhone">
+    <form action = "" method = "post">
+        <div class = "Form">
+            <p>ユーザーネーム</p>
+            <input type = "text" name = "user">
+            <p>メールアドレス</p>
+            <input type = "text" name = "e-mail">
+            <p>パスワード</p>
+            <input type = "text" name = "pass">
+            <p>パスワード（再）</p>
+            <input type = "text" name = "repass">
+            <p>性別</p>
+        </div>
+        <div class = "ra">
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "man">　男　</label>
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "woman">　女　</label>
+            <label><input type = "radio" name = "Gender" style="transform: scale(4.0)" value = "other">　その他　</label>
+        </div>
+    </form><br>
+    <button class = "SignUp">確認</button><br>
+    <br>
+</div>
+</body>
+>>>>>>> origin/main
+</html>
