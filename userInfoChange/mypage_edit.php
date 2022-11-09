05cefa3f58327912438bc7cd2d4661c02920a648
Aso2001028 <83046903+Aso2001028@users.noreply.github.com>
2022-11-09 12:42:03 +0900

    Add files via upload

--- userInfoChange/mypage_edit.php
+++ userInfoChange/mypage_edit.php
@@ -1,8 +1,34 @@
 <?php
+require("./conect.php");
 session_start();
+if (!empty($_POST)) {
+    /* 入力情報の不備を検知 */
+    if ($_POST['mail'] === "") {
+        $error['mail'] = "blank";
+    }
+    if ($_POST['password'] === "") {
+        $error['password'] = "blank";
+    }
+    
+    /* メールアドレスの重複を検知 */
+    if (!isset($error)) {
+        $member = $pdo->prepare('SELECT COUNT(*) as cnt FROM m_user WHERE mail=?');
+        $member->execute(array(
+            $_POST['mail']
+        ));
+        $record = $member->fetch();
+        if ($record['cnt'] > 0) {
+            $error['mail'] = 'duplicate';
+        }
+            
+    if (!isset($error)) {
+        $_SESSION['user'] = $_POST['mail'];   // フォームの内容をセッションで保存
+        exit();
+    }
+ }
 ?>
 <!DOCTYPE html>
-<html lang="ja"></html>
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>マイページ</title>
@@ -20,25 +46,26 @@ session_start();
     <div style="background-color:#505050;">
         <div class="mypage">
            <img src="img/cooltext421486115691405 1.png">
-           </div>
-        </div>
-        <form>
-    <div class="box">
-        <p>ユーザーID</p>
-        <div class="margin"></div>
-        <input type="text" id="id" class="example" size="40px">
-        <p>ユーザー名</p>
-        <div class="margin"></div>
-        <input type="text" id="name" class="example" size="40px">
-        <p>メールアドレス</p>
-        <div class="margin"></div>
-        <input type="email" id="email" class="example" size="40px">
-        <p>パスワード</p>
-        <div class="margin"></div>
-        <input type="password" id="pass" class="example" size="40px">
-        <div class="hoge_button">
-        <button type="submit" onclick="mypage.html" value="遷移">変更完了</button>
         </div>
     </div>
+    <form action="mypage_edit.php" method="post">
+        <div class="box">
+            <p>ユーザーID</p>
+            <div class="margin"></div>
+            <input type="text" id="id" class="example" >
+            <p>ユーザー名</p>
+            <div class="margin"></div>
+            <input type="text" id="name" class="example">
+            <p>メールアドレス</p>
+            <div class="margin"></div>
+            <input type="email" id="email" class="example">
+            <p>パスワード</p>
+            <div class="margin"></div>
+            <input type="password" id="pass" class="example">
+            <div class="hoge_button">
+                <button type="submit" onclick="mypage.html" value="遷移">変更完了</button>
+            </div>
+        </div>
     </form>
 </body>
+</html>
