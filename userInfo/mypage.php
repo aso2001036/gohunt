5c0a06583a7102f34c9ba996083b84b353edab00
Aso2001028 <83046903+Aso2001028@users.noreply.github.com>
2022-11-25 12:45:50 +0900

    Add files via upload

--- userInfo/mypage.php
+++ userInfo/mypage.php
@@ -1,11 +1,18 @@
 <?php
 try {
-    $db = new PDO('mysql:host=mysql207.phy.lolipop.lan;
-dbname=LAA1290637-aso2001028;charaset=utf8',
+    $dbh = new PDO('mysql:host=mysql207.phy.lolipop.lan;
+    dbname=LAA1290637-aso2001028;charaset=utf8',
         'LAA1290637',
         'syun0612');
+
+     $stmt = $dbh->query('SELECT * FROM user');
+
+     $result = 0;
+
+     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
 }   catch (PDOException $e) {
-    echo "データベース接続エラー :".$e->getMessage();
+    echo "データベース接続エラー：".$e->getMessage();
 }
 session_start();
 ?>
@@ -33,16 +40,28 @@ session_start();
         <form action="" method="post">
             <div class="box">
                 <p>ユーザーID</p>
-                <?php echo htmlspecialchars($_POST['user_id']);?>
+                <?php
+                foreach ($result as $user){
+                    echo $user['user_id'];
+                }
+                ?>
                 <hr width="300px">
                 <div class="margin">
                     <p>ユーザー名</p>
-                    <?php echo htmlspecialchars($_SESSION['user_name']);?>
+                <?php
+                foreach ($result as $user){
+                    echo $user['user_name'];
+                }
+                ?>
                 </div>
                 <hr width="300px">
                 <div class="margin">
                     <p>メールアドレス</p>
-                    <?php echo htmlspecialchars($_SESSION['user_mail']);?>
+                <?php
+                foreach ($result as $user){
+                    echo $user['user_mail'];
+                }
+                ?>
                 </div>
                 <hr width="300px">
                 <div class="hoge_button">
