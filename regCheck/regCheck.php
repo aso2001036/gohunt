4355dee51604eb1550fb546ca5f760da927bfe06
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2022-12-07 11:39:11 +0900

    Update regCheck.php

--- regCheck/regCheck.php
+++ regCheck/regCheck.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    // セッション情報の保存
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +10,7 @@
     <title>Gohunt - Check</title>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/regCheck.css">
-</head>
+    </head>
 <body>
 <div class="background">
     <a class="logo">ごはんと</a>
@@ -16,49 +21,65 @@
         <form action="./store_post_insert.php" method="post">
             <a class="store_confirm_text">店舗名</a><br>
             <?php
-             echo '<input type="text" class="store_namekana_box" value="',$_POST[''],'" readonly name="kana"><br>';//valueを変数名に
-             echo '<input type="text" class="store_name_box" value="',$_POST[''],'" readonly name="name"><br>';
+             echo '<input type="text" class="store_namekana_box" value="',$_POST['shopNameKana'],'" readonly name="kana"><br>';//valueを変数名に
+             echo '<input type="text" class="store_name_box" value="',$_POST['shopName'],'" readonly name="name"><br>';
             ?>
             <a class="store_confirm_text">店舗画像</a><br>
             <?php
-              echo '<img class="img" src="./img/',$_POST[''],'><br>';//srcのimg以降のアドレスを変更
+              echo '<img class="img" src="./img/',$_POST['shopImg'],'><br>';//srcのimg以降のアドレスを変更
             ?>
             <a class="store_confirm_text">住所</a><br>
             <?php
-              echo '<input type="text" class="address_box" value="',$_POST[''],'" readonly name="address">';//valueを変数名に
+              echo '<input type="text" class="address_box" value="',$_POST['shopJusyo'],'" readonly name="address">';//valueを変数名に
             ?>
             <div class="star">
                 <a class="store_confirm_text">評価</a><br>
                 <a class="evaluation_text">入りやすさ</a><br>
                 <div class="evaluation">
                     <?php
-                    echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
-                    echo '<input type="hidden" name="appearance" value="',$_POST[''],'">';//valueを変数に
+                    //echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
+                    //echo '<input type="hidden" name="appearance" value="',$_POST[''],'">';//valueを変数に
                     ?>
                 </div>
                 <a class="evaluation_text">味</a><br>
                 <div class="evaluation">
                     <?php
-                    echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
-                    echo '<input type="hidden" name="taste" value="',$_POST[''],'">';//valueを変数に
+                    //echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
+                    //echo '<input type="hidden" name="taste" value="',$_POST[''],'">';//valueを変数に
                     ?>
                 </div>
                 <a class="evaluation_text">雰囲気</a><br>
                 <div class="evaluation">
                     <?php
-                    echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
-                    echo '<input type="hidden" name="atmosphere" value="',$_POST[''],'">';//valueを変数に
+                    //echo '<p><span class="star5_rating" data-rate="',$_POST[''],'"></span></p>';//data-rateの中を変数に
+                    //echo '<input type="hidden" name="atmosphere" value="',$_POST[''],'">';//valueを変数に
                     ?>
                 </div>
             </div>
             <a class="store_confirm_text">タグ</a>
             <div class="tag">
-                <button type="submit" class="tag_button" formaction="(URL 一覧)" form="(フォームID)">タグ</button>
-                <button type="submit" class="tag_button" formaction="(URL 一覧)" form="(フォームID)">タグ</button>
+               <?php
+		require("../conect.php");
+        $max = count($_POST['tagId']);
+                for($i=0; $i<$max; $i++){ 
+                    $tagsid = $_POST['tagId'][$i];
+            $tags = $pdo->prepare('SELECT * FROM m_tag WHERE tag_id = :tag');
+            $tags -> bindValue(':tag',$_POST['tagId'][$i]);
+            $tags -> execute();
+            foreach($tags as $tag){
+		if($i%3==0&&$i!=0){
+            
+			echo '<button type="button" class="tag_button" name="tagId[]" value="',$tagsid,'"><font size="5">',$tag['tag_name'],'</font></button><br>';
+		}else{
+			echo '<button type="button" class="tag_button" name="tagId[]" value="',$tagsid,'"><font size="5">',$tag['tag_name'],'</font></button>';
+		}
+    }
+    }
+    ?>
             </div>
             <a class="store_confirm_text">コメント</a><br>
             <?php
-              echo'<textarea class="store_textarea" name="commemt">',$_POST[''],'</textarea><br>';
+              echo'<textarea class="store_textarea" name="commemt">',$_POST['shopComment'],'</textarea><br>';
             ?>
             <button type="submit"  class="post_button" value="send">投稿する</button>
         </form>
@@ -68,4 +89,4 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="./js/postconfirm.js"></script>
 </body>
-</html>
+</html></em>
