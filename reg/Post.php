e72f226428be1812a638f063069c5d375b2e28d2
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2022-12-07 11:38:28 +0900

    Update and rename Post.html to Post.php

--- reg/Post.php
+++ reg/Post.php
@@ -1,28 +1,33 @@
+<?php
+    session_start();
+    // セッション情報の保存
+
+?>
 <html>
 	<head>
 		  <link rel="stylesheet" href="./css/style.css">
   		  <link rel="stylesheet" href="./css/Post.css">
 		<meta charset="UTF-8">
 		<title>投稿</title>
-	</head>
-<body>
+		<?php require("../header/menu.php"); ?>
 	<div class="page">
 		<div class="title"></div>
+		<form action="../regCheck/regCheck.php" method="post">
 			<div class="post">
 				<a class="shop_name"><font size="10">店舗名</font></a>
 					<div class="kana_box">
-						<input type="text" class="kana" placeholder="仮名入力"></input>
+						<input type="text" class="kana" placeholder="かな入力*ひらがなで入力してください*" name="shopNameKana"></input>
 					</div>
 						<div class="name_input_box">
-							<input type="text" class="name_input" placeholder="店舗名入力">
+							<input type="text" class="name_input" placeholder="店舗名入力" name="shopName">
 						</div> 
 							<div class="shop_img">
 							<p><font size="7">店舗の写真を選択</font><br></div>
-									<div id="preview"><input type="file" onChange="imgPreView(event)"><br>
+									<div id="preview"><input type="file" onChange="imgPreView(event)" name="shopImg"><br>
 									<script src="./js/Post.js"></script></div>
 <div class="locate">
 	<a class="juusyo"><font size="7">お店の場所</font></a><br>
-		<input type="text" class="location" placeholder="住所入力">
+		<input type="text" class="location" placeholder="住所入力" name="shopJusyo">
 </div>
 <div class="map">
 	<script>
@@ -62,22 +67,27 @@
 	</div>
 <div class="tag">
 	<a class="shop_tag"><font size="7">タグ</font></a><br>
-		<button type="button" class="tag_button"><font size="7">タグ1</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ2</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ3</font></button><br>
-		<button type="button" class="tag_button"><font size="7">タグ4</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ5</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ6</font></button><br>
-		<button type="button" class="tag_button"><font size="7">タグ7</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ8</font></button>
-		<button type="button" class="tag_button"><font size="7">タグ9</font></button>
+		<?php
+	require("../conect.php");
+	$tags = $pdo->query('SELECT * FROM m_tag');
+	foreach($tags as $tag){
+		$tata = $tag['tag_id'];
+		if($tag['tag_id']%3==0){
+			echo '<input type="checkbox" class="tag_button" name="tagId[]" value="',$tata,'"><font size="6">',$tag['tag_name'],'</font></button><br>';
+		}else{
+			echo '<input type="checkbox" class="tag_button" name="tagId[]" value="',$tata,'"><font size="6">',$tag['tag_name'],'</font></button>';
+		}
+	}
+		?>
 	
 </div>
 <div><a class="come"><font size="7">コメント</font></a>
-		<p><input type="text" class="comment" placeholder="店舗のコメントを入力"></p>
+		<p><input type="text" class="comment" placeholder="店舗のコメントを入力" name="shopComment"></p>
 		</div>
 </div>
 </div>
+<button type="submit" class = "reg"><font size="6">登録する</font></button><br>
+		</form>
 </div>
 </body>
 </html>
